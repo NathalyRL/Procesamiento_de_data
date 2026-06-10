e51e785da4790b4e91173387b7c3f06376cf3d45
NathalyRL <naty.eli.ramon@gmail.com>
2026-06-09 23:20:57 -0500

    Conversión masiva de mp4 a wav FINAL EXITOSA

--- Conversor_mp4_wav.py
+++ Conversor_mp4_wav.py
@@ -2,15 +2,21 @@ import os
 import subprocess
 from pathlib import Path
 
+# Este código hace uso de la herramienta ffmpeg para convertir archivos .mp4 a .wav
+# Asegúrate de tener ffmpeg instalado y de actualizar la ruta en la configuración masiva. 
+# Luego, simplemente ejecuta este script y se procesarán todos los archivos .mp4 en la 
+# carpeta especificada, guardando los resultados en la carpeta de salida.
+
+
 # ================= CONFIGURACIÓN MASIVA =================
 # 1. Ruta al ejecutable (la misma que usaste en la prueba)
 ruta_ffmpeg = r"D:\Documentos\ffmpeg-2026-04-30-git-cc3ca17127-full_build\bin\ffmpeg.exe"
 
 # 2. Carpeta donde están los 40 videos .mp4
-carpeta_videos = Path(r"D:\Documentos\Ayudante de Investigacion\Archivos_mp4")
+carpeta_videos = Path(r"D:\Documentos\Ayudante de Investigacion\CEL_GO_PRO2")
 
 # 3. Carpeta donde quieres guardar los audios (puede ser la misma o una nueva)
-carpeta_salida = Path(r"D:\Documentos\Ayudante de Investigacion\Codigos\Archivo_wav_convertidos")
+carpeta_salida = Path(r"D:\Documentos\Ayudante de Investigacion\Codigos\Archivos_wav_convertidos_2")
 # ========================================================
 
 # Crear la carpeta de salida si no existe
@@ -23,7 +29,7 @@ errores = 0
 print(f"--- Iniciando conversión masiva en: {carpeta_videos} ---")
 
 # Buscamos todos los archivos .mp4
-for archivo in carpeta_videos.glob("*.mp4"):
+for archivo in carpeta_videos.glob("*.m4a"):
     # Construir el nuevo nombre: nombreOriginal_1.wav
     nuevo_nombre = f"{archivo.stem}_1.wav"
     ruta_salida = carpeta_salida / nuevo_nombre
